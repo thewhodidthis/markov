4f371f7a0cc7eaf3f15895948bae22ca2500c641
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-08 06:58:41 +0100

    test: tweak per latest amends

--- test.js
+++ test.js
@@ -3,8 +3,19 @@
 const { equal, ok } = require('tapeless')
 const markov = require('./')
 
-const filter = markov('I am what I am, you are what you are, they is what they is')
+const source = 'I am what I am, you are what you are, they is what they is'
+const filter = markov(source.split(' '))
 
 equal(typeof filter, 'function', 'thunk on init', 'will return')
 
-ok(filter(), 'will compute')
+const t1 = filter()
+
+ok(t1, null, 'will compute')
+equal(typeof t1, 'string')
+
+const t2 = filter('I')
+
+ok(t2, null, 'will seed as expected')
+ok(t2.includes('am'))
+
+equal(filter('How?'), undefined)
