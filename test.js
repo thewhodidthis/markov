2b76625915be64795e9d00da3a804e8bd86f2e18
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-22 21:36:13 +0000

    test: cleanup

--- test.js
+++ test.js
@@ -6,16 +6,24 @@ const markov = require('./')
 const source = 'I am what I am, you are what you are, they is what they is'
 const filter = markov(source.split(' '))
 
-equal(typeof filter, 'function', 'thunk on init', 'will return')
+equal
+  .describe('thunk on init', 'will return')
+  .test(typeof filter, 'function')
 
 const t1 = filter()
 
-ok(t1, null, 'will compute')
-equal(typeof t1, 'string')
+ok.test(t1)
+
+equal
+  .describe(null, 'will compute')
+  .test(typeof t1, 'string')
 
 const t2 = filter('I')
 
-ok(t2, null, 'will seed as expected')
-ok(t2.includes('am'))
+ok
+  .test(t2)
+  .test(t2.includes('am'))
 
-equal(filter('How?'), undefined)
+equal
+  .describe(null, 'will seed')
+  .test(filter('How?'), undefined)
