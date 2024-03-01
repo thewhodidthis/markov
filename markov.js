e95296e9a453a2e62d1283291076394b05d27441
thewhodidthis <thewhodidthis@fastmail.com>
2024-03-01 10:32:03 +0200

    main/iife: clean up

--- markov.js
+++ markov.js
@@ -12,13 +12,10 @@ var markov = (function() {
     // Dedupe input for lookup keys.
     const keys = [...new Set(body)]
 
-    // Cutoff, save for later.
-    const stop = keys.length - 1
-
     // Gather potential values for each key.
     const data = new Map(
-      keys.map((k, i) => {
-        const v = i >= stop ? keys : body.map((x, j) => (x === k ? body[j + 1] : 0)).filter(x => !!x)
+      keys.map((k, i, a) => {
+        const v = i >= a.length - 1 ? a : body.map((x, j) => (x === k ? body[j + 1] : 0)).filter(x => !!x)
 
         return [k, v]
       }),
